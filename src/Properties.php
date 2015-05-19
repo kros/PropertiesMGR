8c88c4242351c707a99d27fdd6e346ca8bcc3e2c
kros <ormartin@gmail.com>
2015-05-19 10:43:40 +0200

    Add some comments

--- src/Properties.php
+++ src/Properties.php
@@ -5,17 +5,41 @@ require_once 'PropertyNotFoundException.php';
 
 use Kros\PropertiesMGR\PropertyNotFoundException;
 
+/**
+ * Represents an object with one property for each key in a key/value pairs file.
+ */
 class Properties{
 	private $fileName;
 	private $properties=array();
 	
+	/**
+	 * Construct method
+	 * 
+	 * @param string $fileName File name containing the key/value pairs to be loaded.
+	 * @param boolean $sections Optional. If True, each property in the object is a secction of the file.
+	 *                          Every key/value pair is an array element in the right section property. 
+	 * @return Properties Return a Properies object filled with the data in the file $fileName. When using
+	 *                    seccions, each property is an array, So the way to get a property value is like this:
+	 *                    value = object->section['key'].
+	 */
 	public function __construct($fileName, $sections = FALSE){
 		$this->fileName = $fileName;
 		$this->properties = parse_ini_file($this->fileName, $sections);
 	}
+	/**
+	 * Get the file name used to build the object
+	 */
 	public function getFileName(){
 		return $this->fileName;
 	} 
+	/**
+	 * Get a property in the object
+	 * 
+	 * @param string $name Name of the property. When using seccions, each property is an array,
+	 *                     So the way to get a property value is like this:
+	 *                     value = object->section['key']
+	 * @throws PropertyNotFoundException
+	 */
 	public function __get($name) {
         if (array_key_exists($name, $this->properties))
             return $this->properties[$name];
@@ -25,7 +49,12 @@ class Properties{
     public function __isset($name) {
         return array_key_exists($name, $this->properties);
     }		
-	public function getProperty($name){
+	/**
+	 * Get a property in the object
+	 * 
+	 * @param string $name Name of the property.
+	 */
+    public function getProperty($name){
 		return $this->$name;
 	}
 }
